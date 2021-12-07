48c2feb4837aeb9c47ddaeaa8e37605270739b26
Max Dobler <maxdobler77@gmail.com>
2021-12-06 21:38:53 -0500

    DB setup, brewery table, BrewerySQLDAO, Brewery Controller

---
+++ dotnet/Capstone/Models/Brewery.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Models
+{
+    public class Brewery
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; } = "";
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Phone { get; set; }
+        public string Description { get; set; }
+    }
+}
