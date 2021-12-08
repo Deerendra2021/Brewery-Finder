5f71b8d648e4709009a278b19a66434defad3f7a
Jim Penikas <jpenikas@gmail.com>
2021-12-08 15:54:21 -0500

    Brewery Details updates

--- vue/src/views/BreweryDetails.vue
+++ vue/src/views/BreweryDetails.vue
@@ -1,36 +1,49 @@
 <template>
     <div class="brewery-details">
-        <!-- <h1>{{brewery.name}}</h1> -->
+        <h1>{{brewery.name}}</h1>
         <h2>This is the brewery landing page</h2>
         <h3>{{this.$route.params.id}}</h3>
+        <ul>
+            <beer-list-item
+                v-for="beer of beers"
+                v-bind:key="beer.id"
+                v-bind:beer="beer"
+                />
+    </ul>
     </div>
 </template>
 
 <script>
-import BreweryService from '../services/BreweryService'
+// import BeerService from '../services/BeerService'
 
 export default {
-    data() {
-        return {
-            brewery: undefined
-        }
-     },
-        
-    created() {
-        let breweryId = parseInt(this.$route.params.id);
 
-        BreweryService.getBreweryById(breweryId)
-            .then(response => {
-                this.brewery = response.data;
-            })
-            .catch(response => {
-                console.error("Could not find brewery", response);
-                // this.$router.push({name: 'Home'});
-            })
-    }
+    computed: {
+        brewery() {
+            return this.$store.state.breweries.find(b => b.id == parseInt(this.$route.params.id));
+        },
+        beers() {
+            return this.$store.state.beers.filter(b => b.breweryId == parseInt(this.$route.params.id))
+        }
+    },
+        
+    // created() {
+    //     let breweryId = parseInt(this.$route.params.id);
+
+    //     BreweryService.getBeersByBrewery(breweryId)
+    //         .then(response => {
+    //             this.brewery = response.data;
+    //         })
+    //         .catch(response => {
+    //             console.error("Could not find brewery", response);
+    //             // this.$router.push({name: 'Home'});
+    //         })
+    // }
 }
 </script>
 
 <style>
-
+div {
+    color: white;
+}
 </style>
