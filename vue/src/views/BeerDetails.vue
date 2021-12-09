4d7856b880e5d64170533bd3066fb3f72ae676e7
Jim Penikas <jpenikas@gmail.com>
2021-12-09 17:27:11 -0500

    Pulling latest code

--- vue/src/views/BeerDetails.vue
+++ vue/src/views/BeerDetails.vue
@@ -9,7 +9,7 @@
     <p>____________________<p>
     <br />
     <h3>Beer Reviews</h3>
-    <!-- <div class="list-group">
+    <div class="list-group">
         <a href="#" class="list-group-item list-group-item-action flex-column align-items-start active">
             <div class="d-flex w-100 justify-content-between">
                 <h5 class="mb-1">{{reviews.name}}</h5>
@@ -17,7 +17,7 @@
             </div>
             <p class="mb-1">{{reviews.description}}</p>
         </a>
-    </div> -->
+    </div>
 
 </div>
   
