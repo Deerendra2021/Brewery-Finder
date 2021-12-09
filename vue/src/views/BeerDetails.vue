c3371dfefd4eafbd77090f9be7fb6f31a7f8976f
Deerendra Basnet <deerendra_basnet@yahoo.com>
2021-12-09 11:27:25 -0500

    Working on viewing beer information

---
+++ vue/src/views/BeerDetails.vue
@@ -0,0 +1,52 @@
+<template>
+<div class ="beer-details">
+
+    <h1>{{beer.name}}</h1>
+    <h2>{{beer.style}}</h2>
+
+</div>
+  
+</template>
+
+<script>
+
+import BeerService from '../services/BeerService.js';
+
+export default {
+
+    data(){
+        return {
+            beer: undefined,
+        }
+    },
+
+    methods: 
+    {
+        created(){
+            
+           let beerId = parseInt(this.$route.params.id);
+
+           BeerService.getBeerById(beerId)
+           .then(response => {
+
+               console.log(response);
+
+               this.beer = response.data;
+           })
+
+           .catch(error => {
+
+               console.log("Fail to load Beer", error);
+
+           });
+   
+        }
+
+    },
+
+}
+</script>
+
+<style>
+
+</style>
