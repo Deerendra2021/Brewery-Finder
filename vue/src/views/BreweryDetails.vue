db7e332f4a60c3886781b0b21d867a13bbfad73d
Jim Penikas <jpenikas@gmail.com>
2021-12-09 10:25:13 -0500

    Brewery details updates

--- vue/src/views/BreweryDetails.vue
+++ vue/src/views/BreweryDetails.vue
@@ -1,10 +1,14 @@
 <template>
     <div class="container brewery-details">
         <h1>{{brewery.name}}</h1>
-        <h2>This is the brewery landing page</h2>
-        <h3>{{this.$route.params.id}}</h3>
+        <h3>{{brewery.address1}} 
+            <br>
+        {{brewery.city}}, {{brewery.state}} {{brewery.zip}}
+        <br>
+        {{brewery.phone}}</h3>
+        <h3>{{brewery.description}}</h3>
         <ul>
-            <beer-list-item
+            <beer-list-item tag="span" class="d-flex flex-wrap"
                 v-for="beer of beers"
                 v-bind:key="beer.id"
                 v-bind:beer="beer"
@@ -12,33 +16,39 @@
     </ul>
     </div>
 </template>
-
 <script>
-// import BeerService from '../services/BeerService'
+import BeerService from '../services/BeerService'
+import BeerListItem from '../components/BeerListItem'
 
 export default {
+    data() {
+        return {
+            beers: []
+        }
+    },
 
     computed: {
         brewery() {
             return this.$store.state.breweries.find(b => b.id == parseInt(this.$route.params.id));
         },
-        beers() {
-            return this.$store.state.beers.filter(b => b.breweryId == parseInt(this.$route.params.id))
-        }
     },
-        
-    // created() {
-    //     let breweryId = parseInt(this.$route.params.id);
 
-    //     BreweryService.getBeersByBrewery(breweryId)
-    //         .then(response => {
-    //             this.brewery = response.data;
-    //         })
-    //         .catch(response => {
-    //             console.error("Could not find brewery", response);
-    //             // this.$router.push({name: 'Home'});
-    //         })
-    // }
+    components: {
+        BeerListItem
+    },
+
+    created() {
+        let breweryId = parseInt(this.$route.params.id);
+
+        BeerService.getBeersByBrewery(breweryId)
+            .then(response => {
+                this.beers = response.data;
+            })
+            .catch(response => {
+                console.error("Could not find beers", response);
+                // this.$router.push({name: 'Home'});
+            })
+    }
 }
 </script>
 
@@ -46,4 +56,24 @@ export default {
 div {
     color: white;
 }
+
+.brewery-details {
+    display: flex;
+    justify-content: Center;
+    align-items: center;
+    flex-direction: column;
+
+    background-color: #222222a2;
+    max-height: 90%;
+
+    row-gap: 40px;
+
+    color: white;
+    min-height: 74vh;
+    margin-top: 9vh;
+}
+
+/* beer-list-item {
+    display: inline;
+} */
 </style>
