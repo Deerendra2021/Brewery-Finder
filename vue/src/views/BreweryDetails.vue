9568fe92c6e3b539e1f4a1c22ede2fddcf64b344
Julie Kovalishin <juliekovalishin@gmail.com>
2021-12-13 14:05:38 -0500

    adding map feature

--- vue/src/views/BreweryDetails.vue
+++ vue/src/views/BreweryDetails.vue
@@ -1,19 +1,26 @@
 <template>
-    <div class="container brewery-details">
+    <div v-if="brewery" class="container brewery-details">
         <h1>{{brewery.name}}</h1>
         <h3 class="text-center">{{brewery.address1}} 
             <br>
         {{brewery.city}}, {{brewery.state}} {{brewery.zip}}
         <br>
         {{brewery.phone}}</h3>
-        <h3 class="text-center">{{brewery.description}}</h3>
+
+        <div class="mapGrid">
+            <h3 class="text-center">{{brewery.description}}</h3>
+
+            <iframe class="googleMap" width="600" height="450" style="border:0; border: 2px solid black;" loading="lazy" allowfullscreen 
+            v-bind:src="`https://www.google.com/maps/embed/v1/place?q=${addressReturn}&key=${apiKey}`"></iframe>
+        </div>
+
         <ul class="d-inline-flex">
             <beer-list-item class="beer-list"
                 v-for="beer of beers"
                 v-bind:key="beer.id"
                 v-bind:beer="beer"
                 />
-    </ul>
+        </ul>
     </div>
 </template>
 <script>
@@ -23,7 +30,8 @@ import BeerListItem from '../components/BeerListItem'
 export default {
     data() {
         return {
-            beers: []
+            beers: [],
+            apiKey: "AIzaSyA50vzmtIyrL1BsjoTQ1CBJWmdXWp9HuGU"
         }
     },
 
@@ -31,6 +39,12 @@ export default {
         brewery() {
             return this.$store.state.breweries.find(b => b.id == parseInt(this.$route.params.id));
         },
+        addressReturn() {
+
+            const addr = `${this.brewery.name}, ${this.brewery.address1}, ${this.brewery.city}, ${this.brewery.state} ${this.brewery.zip}`;
+
+            return addr;
+        }
     },
 
     components: {
@@ -80,4 +94,33 @@ ul {
 /* beer-list-item {
     display: inline;
 } */
+
+.mapGrid {
+    display: grid;
+    
+    padding-left: 3vh;
+    padding-right: 3vh;
+    grid-template-columns: 1fr 1fr;
+    column-gap: 50px;
+
+    grid-template-areas: 
+        "description map";
+
+    align-items: center;    
+}
+
+.mapGrid > h3 {
+    grid-area: description;
+}
+
+.mapGrid > iframe {
+    grid-area: map;
+    border-radius: 10px;
+}
+
+h1 {
+    font-size: 42px;
+    padding-top: 6vh;
+}
+
 </style>
